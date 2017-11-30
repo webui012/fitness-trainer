98d15f7aecb4d4e904319d8a2375b8a223b2ed9b
webui012 <webui.012@gmail.com>
2017-11-30 12:22:34 +0200

    main page components added

--- fitness-app/src/PropsTest.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PropsTest = props => <div>{props.name}</div>
-
-export default PropsTest;
---
+++ fitness-app/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Footer.scss'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className='footer'>Footer</footer>
+    )
+  }
+}
+
+export default Footer
