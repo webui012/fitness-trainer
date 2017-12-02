7b26d89a31005e226b0f7f4bc473bbadd5a9bb58
scarigor <kurchenkoigor1@gmail.com>
2017-12-02 12:14:39 +0200

    header structure created

--- app/src/components/Header/index.js
+++ app/src/components/Header/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import Logo from '../Logo'
+import Navigation from '../Navigation'
+import Login from '../Login'
 import './style.scss'
 
 const Header = () => {
   return (
-    <header className='header'>header</header>
+    <header className='header'>
+      <Logo />
+      <Navigation />
+      <Login />
+    </header>
   )
 }
 
---
+++ app/src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+const Logo = () => {
+  return (
+    <div className='logo-wrapper'>
+      <img src='' alt='Logo' />
+    </div>
+  )
+}
+
+export default Logo
