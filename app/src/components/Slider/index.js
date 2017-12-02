873d49df3f464ea1124cd24468616c97378d5ae8
scarigor <kurchenkoigor1@gmail.com>
2017-12-02 15:26:06 +0200

    carousel was added

--- app/src/components/Slider/index.js
+++ app/src/components/Slider/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import Swiper from 'react-id-swiper'
 import './style.scss'
 
-const Slider = () => {
-  return (
-    <div className='slider'>slider</div>
-  )
+class Slider extends Component {
+  render() {
+    const params = {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      spaceBetween: 30
+    }
+    return (
+      <Swiper {...params}>
+        <div>
+          <img src='https://media.self.com/photos/5867a314bf5e49f716efe864/2:1/pass/workout-package_1.png' alt=''/>
+        </div>
+
+        <div>
+          <img src='http://cdn-img.health.com/sites/default/files/styles/medium_16_9/public/styles/main/public/gettyimages-498315681.jpg?itok=qqWRWFw9' alt=''/>
+        </div>
+      </Swiper>
+    )
+  }
 }
 
 export default Slider
