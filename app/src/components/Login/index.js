88e05cbbd319027d9459e899bb181ab2252f07cb
scarigor <kurchenkoigor1@gmail.com>
2017-12-02 14:20:54 +0200

    fonts added, header minor styles

--- app/src/components/Login/index.js
+++ app/src/components/Login/index.js
@@ -5,7 +5,6 @@ const Login = () => {
   return (
     <div className='login-section'>
       <a href='#' className='login-btn'>Вход</a>
-      <a href='#' className='login-btn'>Регистрация</a>
     </div>
   )
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './css/reset.scss'
+import './css/fonts.scss'
 import App from './components/App';
 
 render(<App />, document.getElementById('root'));
