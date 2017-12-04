39cdae29ed30cdbc0adcf517b7ab376ed0aa773c
scarigor <kurchenkoigor1@gmail.com>
2017-12-05 01:27:28 +0200

    services section fixed

--- app/src/components/pages/Services/Services.js
+++ app/src/components/pages/Services/Services.js
@@ -3,15 +3,10 @@ import ServiceSection from '../../ServiceSection/ServiceSection'
 import MotiovationSection from '../../MotivationSection/MotiovationSection'
 import styles from './Services.scss'
 
-class Services extends React.Component{
-  render(){
-    return (
-      <div className={styles.services}>
-        <ServiceSection/>
-        <MotiovationSection/>
-      </div>
-    )
-  }
-}
+const Services = props =>
+  <div className={styles.services}>
+    <ServiceSection />
+    <MotiovationSection />
+  </div>
 
 export default Services
