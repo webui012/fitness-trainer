1bc3fc46c1a3825598ac27eee303387e83b6e676
scarigor <kurchenkoigor1@gmail.com>
2017-12-04 03:30:02 +0200

    nav restructured

--- app/src/components/Navigation/index.js
+++ app/src/components/Navigation/index.js
@@ -6,9 +6,31 @@ import {
 
 const Navigation = props =>
   <nav className={styles.nav}>
-    <Link to='/'>Home</Link>
-    <Link to='/contacts'>Contacts</Link>
-    <Link to='/login'>login</Link>
+    <ul className={styles['nav-list']}>
+      <li className={styles['nav-list-item']}>
+        <Link to='/' className={styles.link, styles['nav-link']}>Главная</Link>
+      </li>
+
+      <li className={styles['nav-list-item']}>
+        <Link to='/about' className={styles.link, styles['nav-link']}>О тренере</Link>
+      </li>
+
+      <li className={styles['nav-list-item']}>
+        <Link to='/services' className={styles.link, styles['nav-link']}>Услуги</Link>
+      </li>
+
+      <li className={styles['nav-list-item']}>
+        <Link to='/sales' className={styles.link, styles['nav-link']}>Акции</Link>
+      </li>
+
+      <li className={styles['nav-list-item']}>
+        <Link to='/forum' className={styles.link, styles['nav-link']}>Форум</Link>
+      </li>
+
+      <li className={styles['nav-list-item']}>
+        <Link to='/contacts' className={styles.link, styles['nav-link']}>Контакты</Link>
+      </li>
+    </ul>
   </nav>
 
 export default Navigation
