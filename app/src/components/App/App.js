e1e69d92b3dce2d2cfa8881c1971a239de2c6a55
PShirinov <parviz.shirinov@gmail.com>
2017-12-06 01:17:22 +0200

    create 1on1-online page

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -13,6 +13,7 @@ import Homepage from '../pages/Homepage/Homepage.js';
 import About from '../pages/About/About.js';
 import Services from '../pages/Services/Services.js';
 import Sales from '../pages/Sales/Sales.js';
+import Online from '../pages/Online/Online.js';
 import Forum from '../pages/Forum/Forum.js';
 import Contacts from '../pages/Contacts/Contacts.js';
 
@@ -26,6 +27,7 @@ const App = () =>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/about' component={About} />
         <Route exact path='/services' component={Services} />
+        <Route exact path='/services/online' component={Online} />
         <Route exact path='/sales' component={Sales} />
         <Route exact path='/forum' component={Forum} />
         <Route exact path='/contacts' component={Contacts} />
--- app/src/components/Text/Text.js
+++ app/src/components/Text/Text.js
@@ -2,6 +2,6 @@ import React from 'react';
 import styles from './Text.scss';
 
 const Text = props =>
-  <p className={styles.Text}>{props.content}</p>;
+  <p className={styles.Text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>;
 
 export default Text;
