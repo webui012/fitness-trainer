7e2fc345f15fd5fb9f61a485478a1e14aa99698d
Yuri <zyabel@gmail.com>
2017-12-05 10:58:14 +0300

    Updated branch
    Added contacts page components.
    Installed google-map-react component.
    Need cootdination with DevOps.
    Not worked map(Google API keys).

--- app/src/components/pages/Contacts/Contacts.js
+++ app/src/components/pages/Contacts/Contacts.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React,{ Component } from 'react'
+import styles from './Contacts.scss'
+import ContactsField from '../../ContactsField/ContactsField.js';
+import Map from '../../Map/Map.js';
 
-const Contacts = props =>
-  <div>
-    Contacts
+
+const dataContacts = {
+  adressTitle: 'ADRESS',
+  adress: '123, Rev Avenu, Hawai',
+
+  phoneTitle: 'PHONE',
+  phone: '+81 2345854646',
+
+  emailTitle: 'EMAIL',
+  email: 'email@email.com',
+  
+  storeTitle: 'STORE',
+  store: '123, Rev Avenu, Hawai'
+}
+
+const Contacts = () =>
+  <div className={styles.contacts}>
+
+    <Map />
+
+    <h1>CONTACTS</h1>
+
+    <ContactsField
+      title={dataContacts.adressTitle}
+      data={dataContacts.adress}
+    />
+    <ContactsField
+      title={dataContacts.phoneTitle}
+      data={dataContacts.phone}
+    />
+    <ContactsField
+      title={dataContacts.emailTitle}
+      data={dataContacts.email}
+    />
+    <ContactsField
+      title={dataContacts.storeTitle}
+      data={dataContacts.store}
+    />
   </div>
 
 export default Contacts
+
+
+
+
