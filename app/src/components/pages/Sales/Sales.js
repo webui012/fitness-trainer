c8b3abc9a9407124327acba86c81185d2451537f
webui012 <33353723+webui012@users.noreply.github.com>
2017-12-06 14:15:07 +0200

    sales was added

--- app/src/components/pages/Sales/Sales.js
+++ app/src/components/pages/Sales/Sales.js
@@ -1,1 +1,19 @@
+import React from 'react'
+import SalesItem from '../../SalesItem/SalesItem.js'
 
+const Sales = props =>{
+const salesHeadline = "Lorem";
+const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie accumsan viverra.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie accumsan viverra."
+const expireDate = "01.01.2017"
+const salesImgCaption = "Lorem"
+
+  
+return(
+  <div>
+    <SalesItem salesHeadline={salesHeadline} content={content} expireDate={expireDate} salesImgCaption={salesImgCaption}/>
+    <SalesItem salesHeadline={salesHeadline} content={content} expireDate={expireDate} salesImgCaption={salesImgCaption}/>
+    <SalesItem salesHeadline={salesHeadline} content={content} expireDate={expireDate} salesImgCaption={salesImgCaption}/>
+  </div>
+  )
+}
+export default Sales
