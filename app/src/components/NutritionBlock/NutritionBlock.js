1e9b9bf6568eba8823b68acb7cbccb6cb99c6757
scarigor <kurchenkoigor1@gmail.com>
2017-12-07 00:52:59 +0200

    initial refactoring2

--- app/src/components/NutritionBlock/NutritionBlock.js
+++ app/src/components/NutritionBlock/NutritionBlock.js
@@ -3,10 +3,10 @@ import './NutritionBlock.scss'
 import Text from '../Text/Text.js'
 
 const NutritionBlock = props =>
-        <div className='titlenutr'>
-            <i class='fa fa-sliders fa-2x' aria-hidden='true'></i>
-            <p className='work work-link'>Персонализация</p>
-            <Text content = 'Пользовательские рецепты, соответствующие вашим предпочтениям'/>
-        </div>
+  <div className='titlenutr'>
+    <i class='fa fa-sliders fa-2x' aria-hidden='true'></i>
+    <p className='work work-link'>Персонализация</p>
+    <Text content='Пользовательские рецепты, соответствующие вашим предпочтениям' />
+  </div>
 
 export default NutritionBlock
