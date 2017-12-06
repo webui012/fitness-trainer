c52b165282facd96f9db2ded3de36e3251136f5b
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2017-12-06 20:44:00 +0200

    code refactoring

--- app/src/components/LoginForm/LoginForm.js
+++ app/src/components/LoginForm/LoginForm.js
@@ -19,33 +19,36 @@ class LoginForm extends Component {
   render(){
     return (
       <div className='login-page'>
-        <h2 className='login'> Войти </h2>
 
         <form onSubmit={this.onSubmit}>
+          <fieldset className='login-form'>
+            <legend className='login-form-caption'> Войти </legend>
 
-          <fieldset className='input-area'>
-            <input className='form' type='text'
-                placeholder='Введите имя или адрес эл. почты'
-                onChange={this.handleChange('username')} />
+            <label classname='input-area'>
+              <input className='form' type='text'
+                  placeholder='Введите имя или адрес эл. почты'
+                  onChange={this.handleChange('username')} />
+            </label>
+
+            <label className='input-area'>
+              <input className='form' type='password'
+                  placeholder='Введите пароль'
+                  onChange={this.handleChange('password')} />
+            </label>
+
+            <button className='btn-login' type='submit'>
+              <p className='btn'> Войти </p>
+            </button>
+
+            <label className='link-to'>
+              <Link to='/passwordreset' className='linkto'>Забыли пароль?</Link>
+            </label>
+
+            <label className='link-to'>
+              <Link to='/signup' className='linkto'>Создать аккаунт</Link>
+            </label>
+
           </fieldset>
-
-          <fieldset className='input-area'>
-            <input className='form' type='password'
-                placeholder='Введите пароль'
-                onChange={this.handleChange('password')} />
-          </fieldset>
-
-          <button className='btn-login' type='submit'>
-            <p className='btn'> Войти </p>
-          </button>
-
-          <label className='link-to'>
-            <Link to='' className='linkto'>Забыли пароль?</Link>
-          </label>
-
-          <label className='link-to'>
-            <Link to='/signup' className='linkto'>Создать аккаунт</Link>
-          </label>
         </form>
       </div>
     )
--- app/src/components/Signup/Signup.js
+++ app/src/components/Signup/Signup.js
@@ -21,42 +21,43 @@ class Signup extends Component {
     render(){
     return (
       <div className='signup-page'>
-        <h2 className='signup'>Создать аккаунт</h2>
-
         <form onSubmit={this.onSubmit}>
+          <fieldset className='signup-form'>
+            <legend className='signup-form-caption'>Создать аккаунт</legend>
 
-          <fieldset className='form-input'>
-            <input className='form' type='text'
-                placeholder='Введите ваше имя'
-                onChange={this.handleChange('username')} />
+            <label className='form-input'>
+              <input className='form' type='text'
+                  placeholder='Введите ваше имя'
+                  onChange={this.handleChange('username')} />
+            </label>
+
+            <label className='form-input'>
+              <input className='form' type='text'
+                  placeholder='Введите адрес электронной почты'
+                  onChange={this.handleChange('email')} />
+            </label>
+
+            <label className='form-input'>
+              <input className='form' type='password'
+                  placeholder='Введите пароль'
+                  onChange={this.handleChange('password')} />
+            </label>
+
+            <label className='form-input'>
+              <input className='form' type='password'
+                  placeholder='Повторите пароль'
+                  onChange={this.handleChange('password')} />
+            </label>
+
+            <button className='btn-signup' type='submit'>
+              <p className='btn'>Зарегистрироваться</p>
+            </button>
+
+            <label className='link-to'>
+              <Link to='/login' className='linkto'>Уже есть аккаунт?</Link>
+            </label>
+
           </fieldset>
-
-          <fieldset className='form-input'>
-            <input className='form' type='text'
-                placeholder='Введите адрес электронной почты'
-                onChange={this.handleChange('email')} />
-          </fieldset>
-
-          <fieldset className='form-input'>
-            <input className='form' type='password'
-                placeholder='Введите пароль'
-                onChange={this.handleChange('password')} />
-          </fieldset>
-
-          <fieldset className='form-input'>
-            <input className='form' type='password'
-                placeholder='Повторите пароль'
-                onChange={this.handleChange('password')} />
-          </fieldset>
-
-          <button className='btn-signup' type='submit'>
-            <p className='btn'>Зарегистрироваться</p>
-          </button>
-
-          <p className='do-you-have-account'>
-            <Link to='/login' className='linkto'>Уже есть аккаунт?</Link>
-          </p>
-
         </form>
       </div>
     )
