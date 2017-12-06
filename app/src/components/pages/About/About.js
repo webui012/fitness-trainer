23db24d25a8cfa794acd135c73173ed12adc025c
Sergey <serg.gavrilchenko@gmail.com>
2017-12-06 20:50:48 +0200

    changed package.lock.json, eslintrc and component About

--- app/src/components/pages/About/About.js
+++ app/src/components/pages/About/About.js
@@ -5,8 +5,8 @@ import Text from '../../Text/Text';
 import YouTube from '@u-wave/react-youtube';
 import photo from '../../../images/about_image.jpg';
 
-const AboutWrap = props =>
-  <section className={styles.AboutWrap}>
+const About = props =>
+  <section className={styles.About}>
     <div className={styles.container}>
       <Text content='Биография' textClass={styles.caption} />
       <Image path={photo} imageClass={styles.photo} description='photo' />
@@ -18,4 +18,4 @@ const AboutWrap = props =>
 
 const contentText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-export default AboutWrap;
+export default About;
