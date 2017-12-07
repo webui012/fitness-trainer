2eb6effd39da188825d3de0ba23424423ddfc053
Bogdan Makarenko <bogdan.makarenko@gmail.com>
2017-12-07 11:08:23 +0200

    fixed path dublication

--- app/src/components/ServiceSection/ServiceSection.js
+++ app/src/components/ServiceSection/ServiceSection.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react'
 import Service from '../Service/Service'
 
-import khudaev from '../../images/khudaev.png'
-import khudaev1 from '../../images/training-plan.jpg'
-import khudaev2 from '../../images/khudaev2.png'
-import nutrition from '../../images/nutrition-plan.jpg'
+import onlineTraining from '../../images/khudaev.png'
+import trainingPlan from '../../images/training-plan.jpg'
+import anotherServices from '../../images/khudaev2.png'
+import nutritionPlan from '../../images/nutrition-plan.jpg'
 import './ServiceSection.scss'
 
 const ServiceSection = props =>
   <section className='service-section'>
-    <Service path='online-training' serviceName='Онлайн тренировка 1 на 1' imgSrc='../../../images/khudaev.png' />
-    <Service path='training-plan' serviceName='Индивидуальная программа' imgSrc='../../../images/training-plan.jpg' />
-    <Service path='nutrition-plan' serviceName='Правильное питание' imgSrc='../../../images/nutrition-plan.jpg' />
-    <Service path='' serviceName='Другие услуги' imgSrc='../../../images/khudaev2.png' />
+    <Service path='online-training' serviceName='Онлайн тренировка 1 на 1' imgSrc={onlineTraining} />
+    <Service path='training-plan' serviceName='Индивидуальная программа' imgSrc={trainingPlan} />
+    <Service path='nutrition-plan' serviceName='Правильное питание' imgSrc={nutritionPlan} />
+    <Service path='' serviceName='Другие услуги' imgSrc={anotherServices} />
   </section>
 
 export default ServiceSection
