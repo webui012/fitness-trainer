70ccdb218ecf112df8303361cb8fc10d2cb5918b
Bogdan Makarenko <bogdan.makarenko@gmail.com>
2017-12-07 10:53:15 +0200

    adding Link to services blocks

--- app/src/components/ServiceSection/ServiceSection.js
+++ app/src/components/ServiceSection/ServiceSection.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react'
 import Service from '../Service/Service'
 
 import khudaev from '../../images/khudaev.png'
-import khudaev1 from '../../images/khudaev1.png'
+import khudaev1 from '../../images/training-plan.jpg'
 import khudaev2 from '../../images/khudaev2.png'
+import nutrition from '../../images/nutrition-plan.jpg'
 import './ServiceSection.scss'
 
 const ServiceSection = props =>
   <section className='service-section'>
-    <Service serviceName='Персноальная тренировка 1 на 1' imgSrc='khudaev' />
-    <Service serviceName='Индивидуальная программа' imgSrc='khudaev1' />
-    <Service serviceName='Правильное питание' imgSrc='khudaev2' />
-    <Service serviceName='Заказать на девичник' imgSrc='khudaev' />
+    <Service path='online-training' serviceName='Онлайн тренировка 1 на 1' imgSrc='../../../images/khudaev.png' />
+    <Service path='training-plan' serviceName='Индивидуальная программа' imgSrc='../../../images/training-plan.jpg' />
+    <Service path='nutrition-plan' serviceName='Правильное питание' imgSrc='../../../images/nutrition-plan.jpg' />
+    <Service path='' serviceName='Другие услуги' imgSrc='../../../images/khudaev2.png' />
   </section>
 
 export default ServiceSection
