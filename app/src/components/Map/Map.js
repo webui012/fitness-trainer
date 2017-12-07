423638d825d13a67fd5176d10ce4967c578a1123
zabel <zyabel@gmail.com>
2017-12-07 12:04:03 +0300

    Update Map

--- app/src/components/Map/Map.js
+++ app/src/components/Map/Map.js
@@ -3,6 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import './Map.scss';
 
 
+<<<<<<< Updated upstream
 // const AnyReactComponent = ({ text }) => (<div style={{
 //     position: 'relative', color: 'black', background: 'red',
 //     height: 15, width: 15, top: -20, left: -30, borderRadius: 50
@@ -19,15 +20,26 @@ const AnyReactComponent = ({ text }) => {
     </div>
   )
 }
+=======
+const Marker = ({ text }) => (
+    <div className={styles.marker}>
+        <div className='arrow'></div>
+        <span className={styles.markerSpan}>{text}</span>
+    </div>);
+
+>>>>>>> Stashed changes
 
 class Map extends Component {
-    static defaultProps = {
+    constructor(props){
+      super(props)
+      this.dataLocation = {
         center: { lat: 49.95, lng: 36.33 },
         zoom: 11
-    };
-
+      };
+    }
     render() {
         return (
+<<<<<<< Updated upstream
           <div className='map'>
             <GoogleMapReact
                 bootstrapURLKeys={{ key:'AIzaSyB-7Fn2cGaiF3_2zTedyusFSiYHGn9RfMQ'}}
@@ -41,6 +53,23 @@ class Map extends Component {
               />
             </GoogleMapReact>
           </div>
+=======
+            <div className={styles.map}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key:'AIzaSyB-7Fn2cGaiF3_2zTedyusFSiYHGn9RfMQ'}}
+                    defaultCenter={this.dataLocation.center}
+                    defaultZoom={this.dataLocation.zoom}
+                >
+                    <Marker
+                        /*lat={49.955413}
+                        lng={36.337844}*/
+                        lat={this.dataLocation.center.lat}
+                        lng={this.dataLocation.center.lng}
+                        text={'WorkShop'}
+                    />
+                </GoogleMapReact>
+            </div>
+>>>>>>> Stashed changes
         );
     }
 }
