bb835c7c90efacc840ab3fa587dc6bddeeb6aeea
scarigor <kurchenkoigor1@gmail.com>
2017-12-07 02:00:46 +0200

    initial refactoring

---
+++ app/src/components/pages/Nutrition/Nutrition.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './Nutrition.scss'
+import Slider from '../../Slider/Slider'
+import NutritionBlock from '../../NutritionBlock/NutritionBlock'
+
+const Nutrition = props =>
+  <section className='nutrition'>
+    <Slider />
+    <div className="nutrition-content">
+      <div className="nut-section-row">asd</div>
+      <div className="nut-section-row nut-section-bg"></div>
+    </div>
+  </section>
+
+export default Nutrition
