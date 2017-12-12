381980bdd578dcae35afa3d5e7f019b25c860845
Roman <futorniakroman22@gmail.com>
2017-12-12 23:35:10 +0200

    changed page

---
+++ app/src/pages/TrainingPlanSection/TrainingPlanSectionData.js
@@ -0,0 +1,12 @@
+const TrainingPlanSectionData = {
+    videoId: 'duXyGlVSacM',
+    image: { imgPath: '../../../images/imageTrainingPlan.jpg', description: 'training plan img' },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    slider: {slides:['https://www.hdwallpapers.in/walls/pushup_workout-HD.jpg',
+              'https://media.self.com/photos/5867a314bf5e49f716efe864/2:1/pass/workout-package_1.png',
+              'http://cdn-img.health.com/sites/default/files/styles/medium_16_9/public/styles/main/public/gettyimages-498315681.jpg?itok=qqWRWFw9'],
+              sliderParams:
+    btnText: 'НАЧАТЬ ТРЕНИРОВАТЬСЯ!',
+  }
+
+  export default TrainingPlanSectionData
