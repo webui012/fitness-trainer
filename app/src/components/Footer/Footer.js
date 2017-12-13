d797988bea04451bdbdcd26d661e380689c691b6
scarigor <kurchenkoigor1@gmail.com>
2017-12-13 11:45:04 +0200

    navigation fixed

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -6,9 +6,6 @@ import {
   Logo,
   Navigation
 } from 'Components';
-
-/*import Logo from '../Logo/Logo'
-import Navigation from '../Navigation/Navigation'*/
 import './Footer.scss'
 
 const Footer = () => {
@@ -17,15 +14,7 @@ const Footer = () => {
     <footer className='footer'>
       <Logo style={style} />
       <span className='logo-caption'>Find your perfect body!</span>
-      {/* Need to be presented as a Navigation component */}
-      <nav className='footer-nav'>
-        <a href='#' className='footer-nav-link'>Главная</a>
-        <a href='#' className='footer-nav-link'>О Тренере</a>
-        <a href='#' className='footer-nav-link'>Услуги</a>
-        <a href='#' className='footer-nav-link'>Акции</a>
-        <a href='#' className='footer-nav-link'>Форум</a>
-        <a href='#' className='footer-nav-link'>Контакты</a>
-      </nav>
+      <Navigation />
       <div className='socials'>
         <a href='https://www.facebook.com' className='social-link'>
           <img src={facebook} alt='facebook' />
