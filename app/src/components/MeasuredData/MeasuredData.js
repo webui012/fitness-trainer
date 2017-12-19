fd607a31b58331875b6c11c87d1b43910e2d4594
zabel <zyabel@gmail.com>
2017-12-19 23:09:49 +0300

    Created data.js in About_us container

---
+++ app/src/components/MeasuredData/MeasuredData.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './MeasuredData.scss';
+import {data } from './data';
+
+class MeasuredData extends Component {
+
+}
+
+export default MeasuredData;
