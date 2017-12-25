dee302f6fec97ba3e09c3f0d8e81efeacea7067e
PShirinov <parviz.shirinov@gmail.com>
2017-12-25 17:12:36 +0200

    adding constants for sales page

---
+++ app/src/redux/constants/index.js
@@ -0,0 +1,8 @@
+const ActionTypes = {
+    WAIT_PAGE: 'WAIT_PAGE',
+    DELETE_WAIT_PAGE: 'DELETE_WAIT_PAGE',
+    ADD_SALES: 'ADD_SALES',
+    DELETE_SALES: 'DELETE_SALES'
+};
+
+export default ActionTypes;
