8cc20f60c0011e4ffdaba1e3ddff36ca9c16c381
PShirinov <parviz.shirinov@gmail.com>
2017-12-25 17:21:51 +0200

    adding middleware for sales page

---
+++ app/src/redux/middleware/index.js
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects';
+import sendData from './sendData';
+
+export default function* rootSaga() {
+    yield all([
+        sendData()
+    ])
+}
---
+++ app/src/redux/middleware/sendData.js
@@ -0,0 +1,12 @@
+import { delay } from 'redux-saga';
+import { put, takeEvery, call } from 'redux-saga/effects';
+import actions from '../constants';
+
+export function* sendAsyncData() {
+    yield call(delay, 2500);
+    yield put({ type: 'DELETE_WAIT_PAGE' });
+}
+
+export default function* sendData() {
+    yield takeEvery('WAIT_PAGE', sendAsyncData);
+}
