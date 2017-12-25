c391138b53e1027007ca1d1583033503100b5df8
PShirinov <parviz.shirinov@gmail.com>
2017-12-25 17:18:14 +0200

    adding actions for sales page

---
+++ app/src/redux/actions/index.js
@@ -0,0 +1,10 @@
+import ActionTypes from '../constants';
+
+export const waitPage = () => ({
+    type: ActionTypes.WAIT_PAGE
+});
+
+export const deleteWaitSendData = () => ({
+    type: ActionTypes.DELETE_WAIT_PAGE
+});
+
