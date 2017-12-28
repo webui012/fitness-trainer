6e1fd6f6454b72745cbe95bb1ce3ca5cc1e645fd
Roman <futorniakroman22@gmail.com>
2017-12-28 17:49:34 +0200

    userlayout minor fix

--- app/src/layouts/UserLayout.js
+++ app/src/layouts/UserLayout.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { HeaderCabinet } from 'Components';
 import HeaderCabinetUserData from '../components/HeaderCabinet/HeaderCabinetUserData';
 
-const UserLayout = props => (
+const UserLayout = props =>
   <div className='login-layout'>
     <HeaderCabinet data={HeaderCabinetUserData} />
     <div className='layout-content'>
       {props.children}
     </div>
-    </div>
-  )
+  </div>
 
 
 export default UserLayout;
