a8867e99d02669e10a58a3bed5f8c504fa3b5677
Sergey <serg.gavrilchenko@gmail.com>
2018-01-11 01:26:48 +0200

    Add redux-saga to page About, add key to Router and noscript tag to index.html

--- app/src/redux/middleware/index.js
+++ app/src/redux/middleware/index.js
@@ -2,11 +2,13 @@ import { all } from 'redux-saga/effects';
 import sendData from './sendData';
 import pageLoading from './pageLoading';
 import pageLoadingContacts from './pageLoadingContacts';
+import pageAboutGetStaticData from './page_About_getStaticData';
 
 export default function * rootSaga() {
   yield all([
     sendData(),
     pageLoading(),
-    pageLoadingContacts()
+    pageLoadingContacts(),
+    pageAboutGetStaticData()
   ])
 }
