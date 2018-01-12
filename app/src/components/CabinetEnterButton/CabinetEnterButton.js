c5fd417f8295e31323c3b8798fdbccca916e3af1
Roman <futorniakroman22@gmail.com>
2018-01-12 18:26:24 +0200

    added cabinet link that checks loggin state

---
+++ app/src/components/CabinetEnterButton/CabinetEnterButton.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {userLogin} from '../../redux/actions';
+import {USER, ADMIN} from '../../redux/constants';
+
+import './CabinetEnterButton.scss';
+
+const CabinetEnterButton = props => {
+
+  if (props.userRole === ADMIN){
+    return <Link to='/cabinet/admin' className='cabinet-enter-link'>Кабинет</Link>
+  } else if (props.userRole === USER){
+      return <Link to='/cabinet/user' className='cabinet-enter-link'>Кабинет</Link>
+    }
+  return null;
+}
+
+const mapStateToProps = state => ({
+    userRole: state.login.userRole
+  })
+
+const mapDispatchToProps = dispatch => ({
+  userLogin: role => {
+  dispatch(userLogin(role))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CabinetEnterButton);
