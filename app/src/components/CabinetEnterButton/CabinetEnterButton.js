8cef97f92df72a0421c8530e9ce0b069cc36a6bd
scarigor <kurchenkoigor1@gmail.com>
2018-01-15 22:23:05 +0200

    cabinet button fixed

--- app/src/components/CabinetEnterButton/CabinetEnterButton.js
+++ app/src/components/CabinetEnterButton/CabinetEnterButton.js
@@ -8,9 +8,9 @@ import './CabinetEnterButton.scss';
 const CabinetEnterButton = props => {
 
   if (props.userRole === ADMIN){
-    return <Link to='/cabinet/admin' className='cabinet-enter-link'>Кабинет</Link>
+    return <Link to='/cabinet/admin' className='nav-link'>Кабинет</Link>
   } else if (props.userRole === USER){
-      return <Link to='/cabinet/user' className='cabinet-enter-link'>Кабинет</Link>
+      return <Link to='/cabinet/user' className='nav-link'>Кабинет</Link>
     }
   return null;
 }
