7ac16a4ae8d6d0217c0ea4656bb3d9b7188cff66
scarigor <kurchenkoigor1@gmail.com>
2018-01-15 12:38:22 +0200

    admin layout minor fix

--- app/src/layouts/AdminLayout.js
+++ app/src/layouts/AdminLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from 'Containers';
 import HeaderCabinetAdminData from '../components/HeaderCabinet/HeaderCabinetAdminData';
+// import AdminSidebar from '../components';
 
 const AdminLayout = props => (
   <div className='login-layout'>
     <Header />
+    {/* <AdminSidebar /> */}
     <div className='layout-content'>
       {props.children}
     </div>
