e1fc08067d5e4b8d4cf9666e4ff100fb0a18bcef
PShirinov <parviz.shirinov@gmail.com>
2018-01-30 11:43:09 +0200

    changing styles for sales page

--- server/src/routes/sales.js
+++ server/src/routes/sales.js
@@ -4,41 +4,6 @@ import Sale from '../models/sale';
 
 const router = express.Router();
 
-
-/*const salesData = {
- saleItems: [
- {
- id: 0,
- img: 'https://goo.gl/Lea9B3',
- expireDate: '01.01.2017',
- title: 'До 3 месяцев занятий Pole-Dance',
- description: 'С сертификатом вы получаете 1, 2 или 3 месяца занятий Pole-Dance, Pole Dance Exotic, стрейчинг в школе танцев «Pole Energy» со скидкой до 67%. Предложение актуально для новых клиентов.'
- },
- {
- id: 1,
- img: 'https://goo.gl/oTR8Rs',
- expireDate: '01.01.2017',
- title: 'Безлимитное посещение фитнеса в фитнес-клубе «Сокол»',
- description: 'С сертификатом вы получаете 2 или 3 месяца занятий стретчингом в школе танцев «Masters of Dance»со скидкой до 69%. Предложение актуально только для новых клиентов.'
- },
- {
- id: 2,
- img: 'https://goo.gl/e2aH7N',
- expireDate: '01.01.2017',
- title: 'Разработка и корректировка индивидуальной программы тренировок',
- description: 'Месяц консультаций диетолога и составление плана питания и тренировок для одного или двоих от велнес-клуба «Худей легко» со скидкой до 80%.',
- },
- ]
- }
-
-
-
- router.get('/', (req, res) => {
- return res.status(200).json(salesData);
- })*/
-
-
-
 router.get('/', (req, res) => {
     Sale.find()
         .then(sales => {
@@ -64,14 +29,5 @@ router.post('/', (req, res) => {
         .catch(e => res.status(500).json({ error: e }))
 })
 
-
-/*
- router.get('/sales', (req, res) => {
- test.listCollections(data => {
- console.log(res.json(data));
- }
- )
- })
- */
 
 export default router;
