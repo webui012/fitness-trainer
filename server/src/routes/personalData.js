d43273bb486fc171a41b88d6ce93ae6df3ee3596
Sergey <serg.gavrilchenko@gmail.com>
2018-01-31 19:13:24 +0200

    added the functionality of rewriting photos to disk

--- server/src/routes/personalData.js
+++ server/src/routes/personalData.js
@@ -1,8 +1,9 @@
+import fs from 'fs';
 import express from 'express';
 import mongoose from 'mongoose';
 import PersonalData from '../models/personalData';
 import UserPersonalData from '../models/sendPersonalData';
-import { imagePath } from '../utils/helperFunction';
+import { imagePath, pathToAssets, getFileExtension } from '../utils/helperFunction';
 
 const router = express.Router();
 
@@ -89,18 +90,16 @@ router.post('/metrics/avatar', (req, res) => {
   if (!req.files) {
     return res.sendStatus(400);
   } else {
+    fs.readdir(pathToAssets(), (err, files) => {
+      files.forEach(file => {
+        if (file.includes('5757657')) {
+          fs.unlink(`${pathToAssets()}/${file}`, error => {
+            if (error) { throw error; };
+          });
+        }
+      });
+    });
     req.files.avatar.mv(imagePath(5757657, req.files.avatar.mimetype));
-    // UserPersonalData.find({id: req.user.id})
-    //   .then(user => {
-    //     if (!user.id) { throw new Error(); }
-    //
-    //     req.files.avatar.mv(imagePath(user.id, req.files.avatar.mimetype));
-    //     const imageTypeArray = req.files.avatar.mimetype.split('/');
-    //     const imageType = imageTypeArray[imageTypeArray.length - 1];
-    //     user.avatar.useravatar = `${user.id}.${imageType}`;
-    //     user.save();
-    //   })
-    //   .catch(err => res.sendStatus(500).json({ message: 'User not found' }));
     res.send('ok');
   }
 });
