56697770aa98e40634cb06aeb78fbcf723755ab5
scarigor <kurchenkoigor1@gmail.com>
2018-01-31 16:02:35 +0200

    mongodb url changed

--- server/server.js
+++ server/server.js
@@ -1,24 +1,17 @@
 import express from 'express';
-import mongoose from 'mongoose'; //mongoose for further db dev
-import path from 'path'; // local path for index.html
-import dotenv from "dotenv"; // use external config for PORT
-import bodyParser from 'body-parser'; // middleware for parsing
+import mongoose from 'mongoose';
+import path from 'path';
+import dotenv from "dotenv";
+import bodyParser from 'body-parser';
 
-import users from './src/routes/users';  //routes for users actions (login ...)
-import sales from './src/routes/sales';  //routes for users actions (login ...)
+import users from './src/routes/users';
+import sales from './src/routes/sales';
 
 const app = express();
 
-dotenv.config(); // initialize dotenv config
+dotenv.config()
 
-// mongoose.connect()
-mongoose.connect(
-    process.env.MONGO_ATLAS_HOST +
-    process.env.MONGO_ATLAS_PW +
-    process.env.MONGO_ATLAS_ROUTE,
-    { useMongoClient: true }
-);
-
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true })
 
 app.all('/*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:6289');
@@ -29,9 +22,9 @@ app.all('/*', (req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/users', users) // middleware for users endpoints
-app.use('/api/sales', sales) // middleware for users endpoints
+app.use('/users', users)
+app.use('/api/sales', sales)
 
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "index.html")))
 
-app.listen(process.env.PORT) // listen PORT from .env config
+app.listen(process.env.PORT)
