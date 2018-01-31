780986690488ba9e50811adc578c7c75d284b185
scarigor <kurchenkoigor1@gmail.com>
2018-02-01 01:04:17 +0200

    fixed CORS OPTION request

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ import path from 'path';
 import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 
+// Auth
 import auth from './src/routes/auth';
 import users from './src/routes/users';
-//import orders from './src/routes/orders';
+import orders from './src/routes/orders';
+
+// Pages
 import sales from './src/routes/pages/sales';
 import services from './src/routes/pages/services';
+import serviceOrder from './src/routes/pages/serviceOrder';
 
-import { getServiceOrderPage, postOrder } from './src/routes/serviceOrder';
 
 // Initialize dotenv config
 dotenv.config();
@@ -27,33 +30,25 @@ const app = express();
 // Enable CORS so that we can make HTTP request from webpack-dev-server
 app.all('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
 // Middleware for parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-
-// API routes
-app.route('/cabinet/user/service-order')
-  //get service order page
-  .get(getServiceOrderPage)
-  //create new order
-  .post(postOrder);
-
+// app.use(bodyParser.urlencoded({ extended: true}));
 
 // Routes for user registration and auth
 app.use('/api/auth', auth);
 app.use('/api/users', users)
 
-// Routes for orders
-//app.use('/api/orders', orders)
+// Routes for orders (create / fetch)
+app.use('/api/orders', orders)
 
 // Routes for PAGES
 app.use('/api/sales', sales)
 app.use('/api/services', services)
+app.use('/api/service-order', serviceOrder)
 
 // Default route
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
