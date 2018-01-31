749c1b8156d3591caa88bd285f2f76001fffbe6e
scarigor <kurchenkoigor1@gmail.com>
2018-01-31 23:08:15 +0200

    removed email value in service, added clientID to order

--- server/src/models/serviceOrder.js
+++ server/src/models/serviceOrder.js
@@ -3,14 +3,19 @@ import moment from 'moment-timezone';
 const Schema = mongoose.Schema;
 
 // order schema definition
-const OrderSchema = new Schema(
-  {
-    date: { type: String, default: moment.tz(Date.now(), 'Europe/Kiev').format()},
-    serviceType: String,
-    trainingPurpose: String,
-    client: String
-  }
-);
+const OrderSchema = new Schema({
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  date: {
+    type: String,
+    default: moment.tz(Date.now(), 'Europe/Kiev').format()
+  },
+  serviceType: String,
+  trainingPurpose: String
+})
 
 const collectionName = 'orders';
+
 export default mongoose.model('ServiceOrder', OrderSchema, collectionName);
